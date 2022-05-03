289448eb288f935c975774304ecb682abc52f06c
Bagus <bagus.nurhuda@ui.ac.id>
2022-05-03 19:26:29 +0700

    adding env

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const notes = require('./data/notes')
+const dotenv = require('dotenv')
 const app = express()
-
+dotenv.config()
+const PORT = process.env.PORT
 
 app.get ("/", (req, res) => {
     res.send("API is Running...")
 })
 
-app.get ("/notes", (req, res) => {
-    res.send
+app.get ("/api/notes", (req, res) => {
+    res.json(notes)
 })
+
+app.get ("/api/notes/:id", (req, res) => {
+    console.log(req.params);
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note)
+})
+
 app.get ("/api", (req,res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
 
 
-app.listen (5000, ()=> {console.log(`server is connected to port 5000 `)})
+app.listen (PORT, ()=> {console.log(`server is connected to port ${PORT} `)})
